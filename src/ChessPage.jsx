a300f1d8ccb526a1e334472c7df60107ade1ecc0
IvadiZs <ivadizs@kkszki.hu>
2024-04-10 11:04:13 +0200

    ChessPage done

--- src/ChessPage.jsx
+++ src/ChessPage.jsx
@@ -1,5 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function ChessPage() {
-    return <div>ChessPage</div>;
+    const [chessData, setChessData] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get("https://chess.sulla.hu/chess")
+            .then((response) => {
+                setChessData(response.data);
+                console.log(response.data)
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, [])
+
+    return (
+        <>
+            <h1>Sakkozók</h1>
+            <div className="row">
+                {chessData.map((data) => (
+                    <div className="col-sm-12 col-md-6 col-lg-3 mb-3">
+                        <div className="card p-2" style={{}}>
+                            <img src={data.image_url}></img>
+                            Név: {data.name}<br></br>
+                            Szül. év: {data.birth_date}<br></br>
+                            {data.world_ch_won < 1 ? "" : `${data.world_ch_won}x világbajnok`}
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </>
+    );
 }
